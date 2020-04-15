522608b4b49419bc73139fcc2c1719b4bc8c6bd0
Francinaldo Sousa da Silva <francinalldosilva358@gmail.com>
2020-04-15 19:03:26 -0300

    projeto para testar recursos do Framework

---
+++ Comum.Entity/EntityAbstractConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comum.Entity
+{
+    public abstract class EntityAbstractConfig<TEntidade> : EntityTypeConfiguration<TEntidade>
+        where TEntidade : class
+
+    {
+        public EntityAbstractConfig()
+        {
+            ConfigurarNomeTabela();
+            ConfiguararCamposTabela();
+            ConfigurarChavePrimaria();
+            ConfigurarChaveestrangeira();
+        }
+
+        protected abstract void ConfigurarChaveestrangeira();
+
+        protected abstract void ConfigurarChavePrimaria();
+
+
+        protected abstract void ConfiguararCamposTabela();
+
+
+        protected abstract void ConfigurarNomeTabela();
+      
+    }
+}
