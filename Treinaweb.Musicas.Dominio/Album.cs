f5f0a7c6c85a236deea177819f1bb445e3425e2a
Francinaldo Sousa da Silva <francinalldosilva358@gmail.com>
2020-04-15 19:27:50 -0300

    projeto para testar recursos do Framework

---
+++ Treinaweb.Musicas.Dominio/Album.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treinaweb.Musicas.Dominio
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Ano { get; set; }
+        public string Observacoes { get; set; }
+        public virtual List<Musica> Musicas { get; set; }
+    }
+}
---
+++ Treinaweb.Musicas.Dominio/Musica.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treinaweb.Musicas.Dominio
+{
+    public class Musica
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public virtual Album album { get; set; }
+        public int IdAlbum { get; set; }
+    }
+}
